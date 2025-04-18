d231e1fdb3cc9048938a632c9bd879ef4444ba13
Aleena-22 <292135@ust.com>
2025-04-18 16:25:04 +0530

    task4 done

--- 292135-hackathon4/employeeProject/employees/urls.py
+++ 292135-hackathon4/employeeProject/employees/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.employee_form, name='employee_form'), 
     path('form/', views.employee_form, name='employee_form'),
     path('calculate/', views.calculate_net_salary, name='calculate'),
     path('jumble/', views.jumble_word, name='jumble'),
 ]
+
+
+
